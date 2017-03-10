b26539ed41df8df9fac90b57a0d10c2d43727902
Shelby Smith <ssmith@horizontalintegration.com>
2017-03-09 20:40:33 -0600

    add paypal

--- js/agency.js
+++ js/agency.js
@@ -37,3 +37,19 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+$('.slider-for').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: true,
+ asNavFor: '.slider-nav'
+});
+$('.book-slider').slick({
+ slidesToShow: 3,
+ slidesToScroll: 1,
+ asNavFor: '.slider-for',
+ dots: true,
+ centerMode: true,
+ focusOnSelect: true
+});
