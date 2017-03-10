67d031e617deb3bd2cd10e0ef3eb2a46f4978819
Shelby Smith <ssmith@horizontalintegration.com>
2017-03-09 22:24:00 -0600

    slick slider

--- js/agency.js
+++ js/agency.js
@@ -38,18 +38,23 @@ $('div.modal').on('show.bs.modal', function() {
 	}
 });
 
-$('.slider-for').slick({
- slidesToShow: 1,
- slidesToScroll: 1,
- arrows: false,
- fade: true,
- asNavFor: '.slider-nav'
-});
 $('.book-slider').slick({
- slidesToShow: 3,
- slidesToScroll: 1,
- asNavFor: '.slider-for',
- dots: true,
- centerMode: true,
- focusOnSelect: true
+  slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+
+      }
+    }
+  ]
 });
