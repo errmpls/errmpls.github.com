85cebd938b04582e12a080465a300c8f3d154485
Shelby Smith <ssmith@horizontalintegration.com>
2017-05-11 11:37:38 -0500

    new version Thu May 11 11:37:38 CDT 2017

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 $(window).load( function() {
 
+  var inputs = document.querySelectorAll( '.art-upload' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
+
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( '\\' ).pop();
+
+		if( fileName )
+			label.querySelector( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
+
+
   $('#mycalendar').monthly({
     mode: 'event',
     xmlUrl: '../events.xml'
